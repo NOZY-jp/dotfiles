38acb1bc3102211d30b61eafd08896ce423016cc
nozy-jp <nozyjp.1214@gmail.com>
2026-02-15 12:39:18 +0900

    fix mise path on fish.config

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,15 +1,8 @@
+~/.local/bin/mise activate fish | source
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
 starship init fish | source
 zoxide init fish | source
-/home/nozy/.local/bin/mise activate fish | source
-
-# pnpm
-set -gx PNPM_HOME "/home/nozy/.local/share/pnpm"
-if not string.match -q -- $PNPM_HOME $PATH
-    set -gx PATH "$PNPM_HOME" $PATH
-end
-# pnpm end
-
